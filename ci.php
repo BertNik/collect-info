a6c5872c64049473b6a1ea2f2827982d06128ac8
Nik B <nikolaus.bertino@gmail.com>
2018-09-03 18:34:13 -0400

    check if GET isset

--- ci.php
+++ ci.php
@@ -1,4 +1,11 @@
 <?php
-$fp = fopen('ci_store.json', 'a');
-fwrite($fp, (json_encode($_GET,false)));
-fclose($fp);
+
+$writeToFile = function(){
+    $fp = fopen('ci_store.json', 'a');
+    fwrite($fp, json_encode(array( time() => $_GET['ci']))."\n");
+    fclose($fp);
+};
+if(isset($_GET['ci'])){
+    $writeToFile();
+}
+
